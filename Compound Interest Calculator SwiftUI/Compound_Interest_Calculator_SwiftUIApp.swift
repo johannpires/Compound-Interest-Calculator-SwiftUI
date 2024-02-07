3e8c181a91b8dc33c57214ab2bae1eb9ef5a6c7e
johannpires <johann.pires.stu@gmail.com>
2024-02-07 19:32:23 +0000

    Initial Commit

---
+++ Compound Interest Calculator SwiftUI/Compound_Interest_Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compound_Interest_Calculator_SwiftUIApp.swift
+//  Compound Interest Calculator SwiftUI
+//
+//  Created by Johann Pires on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Compound_Interest_Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compound Interest Calculator SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Compound Interest Calculator SwiftUI
+//
+//  Created by Johann Pires on 07/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
